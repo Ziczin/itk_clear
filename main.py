7ab17db27a139ec64ab167844012610f6d9b9d69
Корнеев Александр Сергеевич <KorneevAS@sood.ru>
2026-04-01 09:09:58 +0400

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ async def read_root():
     logger.info("GET / called")
     return {}
 
-
+#check
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
