c8f7eccdb101999b973fda844a39a1d475cfc1ed
Ziczin <68561269+Ziczin@users.noreply.github.com>
2026-04-20 16:48:51 +0400

    Update request_id_middleware.py

--- src/presentation/middleware/request_id_middleware.py
+++ src/presentation/middleware/request_id_middleware.py
@@ -1,6 +1,6 @@
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
-from src.utils.logger import set_request_id, clear_request_id, get_request_id
+from src.utils.logger import set_request_id, clear_request_id, get_request_id, logger
 
 
 class RequestIdMiddleware(BaseHTTPMiddleware):
@@ -8,11 +8,13 @@ class RequestIdMiddleware(BaseHTTPMiddleware):
 
     async def dispatch(self, request: Request, call_next):
         request_id = request.headers.get("X-Request-ID")
+        logger.debug("Incoming request X-Request-ID: %s", request_id)
         set_request_id(request_id)
 
         try:
             response = await call_next(request)
             response.headers["X-Request-ID"] = get_request_id()
+            logger.debug("Outgoing response X-Request-ID: %s", response.headers.get("X-Request-ID"))
             return response
         finally:
             clear_request_id()
