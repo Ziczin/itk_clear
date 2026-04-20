47f62f3b76e7a8e807c3464936d6865938efea45
Ziczin <68561269+Ziczin@users.noreply.github.com>
2026-04-20 20:17:02 +0400

    Update request_id_middleware.py

--- src/presentation/middleware/request_id_middleware.py
+++ src/presentation/middleware/request_id_middleware.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import Response
@@ -8,31 +9,26 @@ class RequestIdMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         set_request_id(request.headers.get("X-Request-ID"))
         try:
-            req_id = get_request_id()
-
-            req_body = await request.body()
-            logger.info(
-                f"{request.method} {request.url.path}",
-                request_body=req_body.decode("utf-8", errors="replace"),
-            )
-
             response = await call_next(request)
-
-            resp_body = b""
-            async for chunk in response.body_iterator:
-                resp_body += chunk
-
-            logger.info(
-                f"{request.method} {request.url.path} [{response.status_code}]",
-                response_body=resp_body.decode("utf-8", errors="replace"),
-            )
-
-            response.headers["X-Request-ID"] = req_id
-            return Response(
-                content=resp_body,
-                status_code=response.status_code,
-                headers=dict(response.headers),
-                media_type=response.media_type,
-            )
+            if request.url.path not in ("/logs", "/metrics"):
+                req_b = await request.body()
+                resp_b = b"".join([c async for c in response.body_iterator])
+                logger.info(
+                    f"{request.method} {request.url.path}",
+                    request_body=json.loads(req_b),
+                )
+                logger.info(
+                    f"{request.method} {request.url.path} [{response.status_code}]",
+                    response_body=json.loads(resp_b),
+                )
+                response.headers["X-Request-ID"] = get_request_id()
+                return Response(
+                    content=resp_b,
+                    status_code=response.status_code,
+                    headers=dict(response.headers),
+                    media_type=response.media_type,
+                )
+            response.headers["X-Request-ID"] = get_request_id()
+            return response
         finally:
             clear_request_id()
